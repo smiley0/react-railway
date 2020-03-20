b5672eab51f4c1b272e7c7761f7873dfbc5da5f1
Marian Nociar <majo.nociar@gmail.com>
2020-03-20 11:57:37 +0100

    added field for time and date to form

--- src/components/SearchConnections.js
+++ src/components/SearchConnections.js
@@ -7,18 +7,20 @@ class SearchConnections extends React.Component {
         dst_items: [],
         dst: "",
         src: "",
-        seed: "",
-        id: ""
+        id: "",
+        time: "",
+        date: "",
+        test: "FUNGUJE"
     }
 
     handleChange = (e) => {
         console.log("handleChange"+e.target.id)
         console.log("handleChange"+e.target.value)
         if(e.target.id === "dst"){
-            this.setState({dst: e.target.value, seed: e.target.id})
+            this.setState({dst: e.target.value})
         }
         else if(e.target.id === "src"){
-            this.setState({src: e.target.value, seed: e.target.id})
+            this.setState({src: e.target.value})
         }
         
         if(e.target.value.length === 0 && e.target.id === "dst"){
@@ -68,6 +70,35 @@ class SearchConnections extends React.Component {
         })
     }
 
+    addTimeDate = () => {
+        console.log("Add time was called!!!!!")
+        var date = new Date();
+        var m = date.getMinutes();
+        var h = date.getHours();
+        var d = date.getDate();
+        var mo = date.getMonth();
+        var y = date.getFullYear();
+        this.setState({
+            time: h + ":" + m,
+            date: d + "." + mo + "." + y 
+        })
+    }
+
+    handleChangeTD = (e) => {
+        console.log("eTD"+e.target.id)
+        console.log("eTD"+e.target.value)
+        if(e.target.id === "time"){
+            this.setState({time: e.target.value})
+        }
+        else if(e.target.id === "date"){
+            this.setState({date: e.target.value})
+        }
+    }
+
+    componentDidMount(){
+        this.addTimeDate();
+    }
+
 
     render(){
         return(
@@ -79,9 +110,12 @@ class SearchConnections extends React.Component {
                     <input autoComplete="off" type="text" id="src" onChange={this.handleChange} value={this.state.src}></input>
                     <SearchOptions addToDst={this.addToSrc} options={this.state.src_items}></SearchOptions>
                     <label htmlFor="dst">Kam:</label>
-                    <input autoComplete="off" type="text" id="dst" onChange={this.handleChange} value={this.state.dst}></input>
-                
+                    <input autoComplete="off" type="text" id="dst" onChange={this.handleChange} value={this.state.dst}></input>                
                     <SearchOptions addToDst={this.addToDst} options={this.state.dst_items}></SearchOptions>
+                    <label htmlFor="date">Datum:</label>
+                    <input autoComplete="off" type="text" id="date" onChange={this.handleChangeTD} value={this.state.date}></input>
+                    <label htmlFor="time">Cas:</label>
+                    <input autoComplete="off" type="text" id="time" onChange={this.handleChangeTD} value={this.state.time}></input>
                     <button>Submit</button>
                 </form>
             </div>
