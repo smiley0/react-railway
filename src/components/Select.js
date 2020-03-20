1d15687188c4e3156e330e951ea924fa18ff9f98
Marian Nociar <majo.nociar@gmail.com>
2020-03-20 17:51:00 +0100

    compounding url for fetching data

--- src/components/Select.js
+++ src/components/Select.js
@@ -2,12 +2,28 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 class Select extends React.Component {
+    state = {
+        url: "",
+    }
+
+    componentDidMount(){
+        this.setState(
+            {
+                url: "?src="+this.props.searchInfo.src + "&dst=" + this.props.searchInfo.dst + "&time=" + this.props.searchInfo.time,
+            }
+        )
+    }
+
     render(){
         console.log(this.props)
+        console.log(this.props.searchInfo)
+        console.log(this.props.searchInfo.src)
+        //?time=8:00&src=Krivan&dst=Bratislava
+
         return(
             <div>
                 <h1> Select </h1>
-                <p>Tu sa vyberie z ponuky vlakov</p>
+                <p>{this.state.url}</p>
             </div>
         )
     }
