6166ad1f3f10667d4d8b1e7603e1603db3a5ba2f
Marian Nociar <majo.nociar@gmail.com>
2020-03-21 19:25:38 +0100

    css for navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import SearchConnections from './SearchConnections'
+import './Home.css'
 
 const Home = () => {
     return(
-        <div className='container'>
-            <h2>Home</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-            <SearchConnections></SearchConnections>
+        <div className='home'>
+            <div className='bg'>
+            <div className='center'>
+                <h2>Home</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                <SearchConnections></SearchConnections>
+            </div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import './Navbar.css'
+import logo from './logo.png'
 
 const Navbar = () => {
     return(
         <nav id='navbar'>
-            <div className='menu'>
-                <div className='menu-left'>
+            <div className='center'>
+                <div className='left'>
+                    <NavLink to='/'><img src={logo} alt="logo"></img></NavLink>                        
                     <ul>
-                        <li><NavLink to='/'><span>Railway Co.</span></NavLink></li>
-                        <li><NavLink to='/about'>About</NavLink></li>
-                    </ul>   
-                </div>
-                <div className='menu-right'>
-                    <ul>
-                        <li><NavLink to='/register'>Register</NavLink></li>
-                        <li><NavLink to='/login'>Login</NavLink></li>
+                        <li><span>SK</span></li>
+                        <li>/</li>
+                        <li><span>EN</span></li>
                     </ul>
+                </div>
+                <div className='right'>
+                    <button className="register">Register</button>
+                    <button>Login</button>
                 </div>
             </div>
         </nav>
