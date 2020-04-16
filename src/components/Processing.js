96852d11ae5188121e0e9955009dafa2ced7bf7f
Marian Nociar <majo.nociar@gmail.com>
2020-04-16 14:39:33 +0200

    in progress, I had to leave

--- src/components/Processing.js
+++ src/components/Processing.js
@@ -19,13 +19,16 @@ class Processing extends React.Component {
                     passenger_id: "",
                 }
             ]
-        }
+        },
+        haveResponse: false,
+        response: {},
     }
 
     componentDidMount = () => {
         var post = {...this.state.post}
         post.price = this.props.price;
-        post.valid_on = this.props.searchInfo.date;
+        let dateSlice = this.props.searchInfo.date.split(".");
+        //post.valid_on = dateSlice[2]+"-"++;
         this.props.passengers.forEach((element, i) => {
             console.log("passenger")
             console.log(element)
@@ -38,18 +41,46 @@ class Processing extends React.Component {
             post.segments[i] = {start: element.fromID, end: element.toID}
         });
         this.setState({post})
+        //this.postTicket(post)
+    }
+
+    postTicket = (post) => {
+        console.log("POST TICKET METHOD")
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(post)
+        };
+        console.log(requestOptions)
+        fetch('http://127.0.0.1:8000/ticket/', requestOptions)
+            .then(res => res.json())
+            .then(json => this.setState({
+                haveResponse: true,
+                response: json,
+            }));
     }
 
     render(){
         console.log("Processing")
         console.log(this.state)
-        return(
-            <div className="center processing">
-                <h1> Processing !!!!!!!!!! </h1>
-                <h2> cena: {this.props.price}</h2>
-                <p>{this.props.searchInfo.date}</p>
-            </div>
-        )
+        if(this.state.haveResponse){
+            console.log("RESPONSE")
+            console.log(this.state.response)
+            return(
+                <div>
+                    <h1> we have a response</h1>
+                </div>
+            )
+        }
+        else {
+            return(
+                <div className="center processing">
+                    <h1> Processing !!!!!!!!!! </h1>
+                    <h2> cena: {this.props.price}</h2>
+                    <p>{this.props.searchInfo.date}</p>
+                </div>
+            )
+        }
     }
 }
 
