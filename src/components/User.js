5366cfe75bdf190bf0aa263d5d50af8ee036cb6e
Marian Nociar <majo.nociar@gmail.com>
2020-04-19 18:02:47 +0200

    display informations about user tickets

--- src/components/User.js
+++ src/components/User.js
@@ -46,9 +46,23 @@ class User extends React.Component {
         if(this.state.token !== ""){
             console.log(this.state.token)
             console.log(this.state.response)
+            let ticketsList = null;
+            if(this.state.haveResponse){
+            ticketsList = this.state.response.results.map((ticket, i) => {
+                return(
+                      <li key={i}>
+                        <Ticket ticket={ticket}></Ticket>
+                      </li>
+                )
+              })
+            }
             return(
                 <div>
                     <h1>Hello {this.state.uname}</h1>
+                    <h2>Zakupene listky</h2>
+                    <ul>
+                        {this.state.haveResponse? ticketsList: null}
+                    </ul>
                 </div>
             )
         }
@@ -71,6 +85,121 @@ const mapStateToProps = (state) => {
 
 export default connect(mapStateToProps)(User)
 
+class Ticket extends React.Component {
+    state = {
+        showMore: false,
+    }
+    showMore = () => {
+        this.setState({showMore: !this.state.showMore})
+    }
+    render(){
+        const ticket = this.props.ticket
+        console.log()
+        return(
+            <div onClick={this.showMore}>
+                <p>{ticket.segments[0].start.station_name} - 
+                {ticket.segments[ticket.segments.length - 1].end.station_name}</p>
+                <p>{ticket.valid_on} cena: {ticket.price}</p>
+                {ticket.status === 'U'?<a href={ticket.paygate_link}>Zaplatit</a>: null}
+                {this.state.showMore? 
+                    <div>
+                        <Segments passengers={ticket.passengers} reservations={ticket.reservations} segments={ticket.segments}></Segments>
+                    </div>
+                : null}
+            </div>
+        )
+    }
+}
 
+class Segments extends React.Component {
+    render(){
+        const segments = this.props.segments
+        const segment = segments.map((segment, i) => {
+            return(
+                  <li key={i}>
+                    <div>
+                        <p>{segment.start.station_name} {segment.start.departure_time}</p>
+                        <p>{segment.end.station_name} {segment.end.arrival_time}</p>
+                        <ul>
+                            <Reservations passengers={this.props.passengers} id={segment.id} reservations={this.props.reservations}></Reservations>
+                        </ul>
+                    </div>
+                  </li>
+            )
+          })
+        return(
+            <div>
+                <ul>
+                    {segment}
+                </ul>
+            </div>
+        )
+    }
+}
 
-                                
+class Reservations extends React.Component {
+    render(){
+        const reservations = this.props.reservations;
+        const id = this.props.id;
+        let sReservations = null;
+        console.log(reservations)
+        console.log(id)
+        if(reservations.length > 0){
+            let reservationsForSegment = reservations.filter((value) => {
+                return value.train_segment === id;
+            })
+            sReservations = reservationsForSegment.map((reservation, i) => {
+                return(
+                    <li key={i}>
+                        <ReservationInfo passengers={this.props.passengers} seat={reservation.seat_number} carriage={reservation.carriage} passengerID={reservation.passenger}></ReservationInfo>
+                    </li>
+                )
+            })
+        }
+        return(
+            <div>
+                <ul>
+                    {sReservations}
+                </ul>
+            </div>
+        )
+    }
+}
+ 
+class ReservationInfo extends React.Component {
+    state = {
+        seat: "",
+        carriage: "",
+        passenger: "",
+    }
+    componentDidMount = () => {
+        fetch(this.props.carriage)
+        .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    carriage: json.number,
+                })
+            });
+        console.log(this.props.passengers)
+        console.log(this.props.passengerID)
+
+        let passenger = this.props.passengers.find(value => {
+            return value.id === this.props.passengerID
+        })
+        console.log(passenger)
+        this.setState({
+            seat: this.props.seat,
+            passenger: passenger.first_name + ' ' + passenger.last_name,
+        })
+        
+    }
+    render(){
+        return(
+            <div>
+                <p>sedadlo: {this.state.seat}</p>
+                <p>vozen: {this.state.carriage}</p>
+                <p>uzivatel: {this.state.passenger}</p>
+            </div>
+        )
+    }
+}
